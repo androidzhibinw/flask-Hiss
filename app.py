34586bf232a96653ceee2a02dbd80bbd11745401
Zhibin Wang <android_zhibinw@163.com>
2015-12-08 17:11:56 +0800

    add item detail page.
    Change-Id: I9b3dd447b42c3ab8987e9ba9637e74feaee9de75

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-import model
 
 
 app = Flask(__name__)
@@ -9,10 +8,10 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
-    items = model.Items.query.all()
-    return render_template('home.html',items=items)
+from view import items_blueprint
+
+app.register_blueprint(items_blueprint)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- view.py
+++ view.py
@@ -1,5 +1,19 @@
-from app import app
+from flask import render_template, Blueprint,\
+        request,flash, redirect, url_for
 
-@app.route('/')
-def index():
-    return 'hello 123'
+items_blueprint = Blueprint(
+        '/',__name__,
+        template_folder='templates'
+        )
+
+
+from model import Items
+@items_blueprint.route('/')
+def home():
+    items = Items.query.all()
+    return render_template('home.html',items=items)
+
+@items_blueprint.route('/item/<id>')
+def item(id=None):
+    item = Items.query.get(id)
+    return render_template('item.html',item=item)
