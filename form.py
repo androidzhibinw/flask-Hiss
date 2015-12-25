539b9bcee3f75b63d268bb7ef13ed45844ee1644
Zhibin Wang <android_zhibinw@163.com>
2015-12-25 17:57:33 +0800

    add more fields.
    Change-Id: I37b09a953ac004148e4c92deb0390ded35c3eb02

--- form.py
+++ form.py
@@ -1,6 +1,13 @@
 from flask_wtf import Form
-from wtforms import TextField
+from wtforms import StringField,TextAreaField
 from wtforms.validators import DataRequired
 
 class ItemForm(Form):
-    title = TextField('Title',validators=[DataRequired()])
+    title = StringField('Title',validators=[DataRequired()])
+    reproduce_steps = TextAreaField('Reproduce Steps',validators=[DataRequired()])
+    crs = StringField('CRs')
+    jira = StringField('Jiras')
+    log_analysis = TextAreaField('Log Analysis')
+    solution_desc = TextAreaField('Solution')
+    gerrits = StringField('Gerrits')
+
