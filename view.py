019016a7d16b1fd537e1c5d77103affee48ed415
Zhibin Wang <android_zhibinw@163.com>
2015-12-30 17:06:10 +0800

    add update function.
    Change-Id: Ide9c705551bb8248937b7bb6e0b9b4f645e5f015

--- view.py
+++ view.py
@@ -16,11 +16,43 @@ from app import db
 def home():
     items = Items.query.all()
     return render_template('home.html',items=items)
-#view one , update
+#view one,
 @items_blueprint.route('/item/<id>')
 def item(id=None):
     item = Items.query.get(id)
     return render_template('item.html',item=item)
+#updtae
+@items_blueprint.route('/item/<id>/update',methods=['GET','POST'])
+def item_update(id=None):
+    item = Items.query.get(id)
+    form = ItemForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #print 'update post',form.title,form.reproduce_steps,form.crs,form.jiras,form.log_analysis,form.solution_desc,form.gerrits
+        item.title = form.title.data
+        item.reproduce_steps = form.reproduce_steps.data
+        item.crs = form.crs.data
+        item.jiras = form.jiras.data
+        item.log_analysis = form.log_analysis.data
+        item.solution_desc = form.solution_desc.data
+        item.gerrits = form.gerrits.data
+        try:
+            db.session.commit()
+            flash("update "+ id + " success!")
+        except Exception as e:
+            flash("update:" +str(e))
+        return redirect(url_for('.home'))
+    else:
+        if not item:
+            flash("id=" + id + " does not exist..")
+            return redirect(url_for('.home'))
+        form.title.data = item.title
+        form.reproduce_steps.data=item.reproduce_steps
+        form.crs.data = item.crs
+        form.jiras.data = item.jiras
+        form.log_analysis.data = item.log_analysis
+        form.solution_desc.data = item.solution_desc
+        form.gerrits.data = item.gerrits
+        return render_template('update.html', form=form,item=item)
 #add 
 @items_blueprint.route('/item/add',methods=['GET','POST'])
 def item_add():
